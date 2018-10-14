047d1212f65742ba255ffe7b8f25f5a4f5585a2b
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-14 18:43:28 +0530

    Test added related to nigative number

--- test/selectEvenOrOddNumbersTest.js
+++ test/selectEvenOrOddNumbersTest.js
@@ -2,9 +2,21 @@ let assert = require('assert');
 let lib = require('../.lib/selectEvenOrOddNumbers.js');
 let countNumbers = require('../.lib/count.js');
 
+//only one even number
+let actualOutput = lib.selectEven([2]);
+let expectedOutput = [2];
+assert.deepEqual(actualOutput,expectedOutput);
+assert.equal(1,countNumbers.count(expectedOutput));
+
+//negative numbers
+actualOutput = lib.selectEven([-2,-4,-5]); 
+expectedOutput = [-2,-4];
+assert.deepEqual(actualOutput,expectedOutput);
+assert.equal(2,countNumbers.count(expectedOutput));
+
 //combine both even and odd numbers
-let actualOutput =lib.selectEven([2,6,9,3,5]) ;
-let expectedOutput = [2,6];
+actualOutput =lib.selectEven([2,6,9,3,5]) ;
+expectedOutput = [2,6];
 assert.deepEqual(actualOutput,expectedOutput);
 assert.equal(2,countNumbers.count(expectedOutput)); 
 
@@ -14,11 +26,23 @@ expectedOutput = [12,24,46,68,80];
 assert.deepEqual(actualOutput,expectedOutput);
 assert.equal(5,countNumbers.count(expectedOutput)); 
 
-//only Even numbers
+//only odd numbers
 actualOutput = lib.selectEven([23,15,1,47,79]);
 expectedOutput = [];
 assert.deepEqual(actualOutput,expectedOutput);
 assert.equal(0,countNumbers.count(expectedOutput)); 
+
+//only one odd number
+actualOutput = lib.selectOdd([3]);
+expectedOutput = [3];
+assert.deepEqual(actualOutput,expectedOutput);
+assert.equal(1,countNumbers.count(expectedOutput));
+
+//negative numbers
+actualOutput = lib.selectOdd([-2,-4,-5]); 
+expectedOutput = [-5];
+assert.deepEqual(actualOutput,expectedOutput);
+assert.equal(1,countNumbers.count(expectedOutput));
 
 //combine both even and odd numbers
 actualOutput =lib.selectOdd([2,6,9,3,5]) ;
