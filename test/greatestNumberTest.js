949a627b2708a3936df8f8584d7963108bf18e5a
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-14 23:12:05 +0530

    Included test for one element array

--- test/greatestNumberTest.js
+++ test/greatestNumberTest.js
@@ -3,7 +3,12 @@ const lib = require('../.lib/GreatestNumber.js');
 
 //valid input
 let actualOutput = lib.findGreatestNumber([16,30,56,0]);
-let expectedOutput = 56
+let expectedOutput = 56;
+assert.equal(actualOutput,expectedOutput);
+
+//only one element in array
+actualOutput = lib.findGreatestNumber([6]);
+expectedOutput = 6
 assert.equal(actualOutput,expectedOutput);
 
 //empty array as an input
@@ -15,3 +20,9 @@ assert.equal(actualOutput,expectedOutput);
 actualOutput = lib.findGreatestNumber([1,2,5,8,1]);
 expectedOutput = 8;
 assert.equal(actualOutput,expectedOutput);
+
+//negative numbers
+actualOutput = lib.findGreatestNumber([-1,-3,-45,5,9,1]);
+expectedOutput = 9;
+assert.equal(actualOutput,expectedOutput);
+
