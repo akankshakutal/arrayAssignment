ca10bc5cb15be9694f10859f28eadc0d6b6e6170
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-14 18:17:18 +0530

    Some test are added
    test related to float is added
    test related to same number and zero is added
    
    digits after decimal point are limit to two that should be change

--- test/averageOfElementsTest.js
+++ test/averageOfElementsTest.js
@@ -15,3 +15,20 @@ assert.equal(isNaN(actualOutput),isNaN(expectedOutput));
 actualOutput = lib.average([6]);
 expectedOutput = 6;
 assert.equal(actualOutput,expectedOutput);
+ 
+//take only one element 
+assert.equal(lib.average([2]),2);
+
+//check with 0
+assert.equal(lib.average([0,2]),1);
+
+//check with negative numbers
+assert.equal(lib.average([1,-5]),-2);
+
+//check with same number
+assert.equal(lib.average([1,1]),1);
+
+//check with decial point
+assert.equal(lib.average([3,5,6,2,9,4]),4.83);
+
+
