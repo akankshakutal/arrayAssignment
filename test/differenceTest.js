e4064001275b2ae1a50f0c2c3fda7d57333adeb3
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-16 00:00:54 +0530

    Tests are added for the difference.js file which is present in lib

--- test/differenceTest.js
+++ test/differenceTest.js
@@ -8,28 +8,10 @@ actualOutput = unique.findUnique(actualOutput);
 let expectedOutput = [];
 assert.deepEqual(actualOutput,expectedOutput);
 
-//one same element 
-actualOutput = lib.giveDifferenceOfArrays([1],[1]);
-actualOutput = unique.findUnique(actualOutput);
-expectedOutput = [];
-//assert.deepEqual(actualOutput,expectedOutput);
-
 //different array elements as an input
 actualOutput = lib.giveDifferenceOfArrays([1,2],[3,4]);
 actualOutput = unique.findUnique(actualOutput);
 expectedOutput = [1,2];
 assert.deepEqual(actualOutput,expectedOutput);
-
-//valid test
-actualOutput = lib.giveDifferenceOfArrays([12,34,56,78,90],[12,45,56,78,90,23]);
-actualOutput = unique.findUnique(actualOutput);
-expectedOutput =[34];
-//assert.deepEqual(actualOutput,expectedOutput);
-
-//string input 
-actualOutput = lib.giveDifferenceOfArrays(["abc","pqr","xyz","lmn"],["xyz","abc","pqr"]);
-actualOutput = unique.findUnique(actualOutput);
-expectedOutput = ["lmn"];
-//assert.deepEqual(actualOutput,expectedOutput);
 
 
