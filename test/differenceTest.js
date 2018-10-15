dc59d200edef3fd9ec758f85781ea5b8e551c561
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-16 00:39:05 +0530

    Tests are added ffor one element array and string

--- test/differenceTest.js
+++ test/differenceTest.js
@@ -14,4 +14,21 @@ actualOutput = unique.findUnique(actualOutput);
 expectedOutput = [1,2];
 assert.deepEqual(actualOutput,expectedOutput);
 
+//one same element 
+actualOutput = lib.giveDifferenceOfArrays([1],[1]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput = [];
+assert.deepEqual(actualOutput,expectedOutput);
+
+//valid test
+actualOutput = lib.giveDifferenceOfArrays([12,34,56,78,90],[12,45,56,78,90,23]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput =[34];
+assert.deepEqual(actualOutput,expectedOutput);
+
+//string input 
+actualOutput = lib.giveDifferenceOfArrays(["abc","pqr","xyz","lmn"],["xyz","abc","pqr"]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput = ["lmn"];
+assert.deepEqual(actualOutput,expectedOutput);
 
