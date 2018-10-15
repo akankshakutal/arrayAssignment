067752a9ed7762792106ddf16c7e3bf3e7fe9393
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-15 23:55:51 +0530

    tTests are added for the difference.js file which is present in lib

---
+++ test/differenceTest.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const lib = require('../lib/difference.js');
+const unique = require('../lib/uniqueElements.js');
+
+//empty array as an input
+let actualOutput = lib.giveDifferenceOfArrays([],[]);
+actualOutput = unique.findUnique(actualOutput);
+let expectedOutput = [];
+assert.deepEqual(actualOutput,expectedOutput);
+
+//one same element 
+actualOutput = lib.giveDifferenceOfArrays([1],[1]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput = [];
+//assert.deepEqual(actualOutput,expectedOutput);
+
+//different array elements as an input
+actualOutput = lib.giveDifferenceOfArrays([1,2],[3,4]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput = [1,2];
+assert.deepEqual(actualOutput,expectedOutput);
+
+//valid test
+actualOutput = lib.giveDifferenceOfArrays([12,34,56,78,90],[12,45,56,78,90,23]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput =[34];
+//assert.deepEqual(actualOutput,expectedOutput);
+
+//string input 
+actualOutput = lib.giveDifferenceOfArrays(["abc","pqr","xyz","lmn"],["xyz","abc","pqr"]);
+actualOutput = unique.findUnique(actualOutput);
+expectedOutput = ["lmn"];
+//assert.deepEqual(actualOutput,expectedOutput);
+
+
