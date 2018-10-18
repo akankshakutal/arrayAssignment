9ec1d3ab9ddc70c331f078f00eedb5e4265dde80
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-18 15:45:32 +0530

    tests are changed according to the functions

--- test/arrayOperationsTest.js
+++ test/arrayOperationsTest.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const lib = require('../lib/arrayOperations.js');
-const addElement = lib.addElement,deleteElement=lib.deleteElement,sortArray=lib.sortArray;
+const addElement = lib.addElement,deleteElement=lib.deleteElement
+const ascendingOrder = lib.ascendingOrder;copyArray = lib.copyArray
+const descendingOrder = lib.descendingOrder;
 
 //inserting in the middle
 let actualOutput = addElement([1,2,3,4],10,2);
@@ -32,22 +34,20 @@ actualOutput = deleteElement([100,200,300,600],600);
 expectedOutput = [100,200,300,];
 assert.deepEqual(actualOutput,expectedOutput);
 
-
-actualOutput = sortArray("ascending",[34,68,12,56,90]);
+//sort elements by ascending order
+actualOutput = ascendingOrder([34,12,56,90,68]);
 expectedOutput = [12,34,56,68,90];
 assert.deepEqual(actualOutput,expectedOutput);
 
 //sort we have same elements  
-actualOutput = sortArray("ascending",[3,6,8,3,9]);
+actualOutput = ascendingOrder([3,6,8,3,9]);
 expectedOutput = [3,3,6,8,9];
 assert.deepEqual(actualOutput,expectedOutput);
 
 //sort does not edit input
 let input = [30,67,56,12,90];
-actualOutput = sortArray("descending",input);
+actualOutput = descendingOrder(input);
 expectedOutput = [90,67,56,30,12];
 assert.deepEqual(actualOutput,expectedOutput);
 assert.deepEqual(input,[30,67,56,12,90]);
 
-//sort ignores non recognized orders
-assert.deepEqual(sortArray('random',[5,3,2,1]),[5,3,2,1]);
