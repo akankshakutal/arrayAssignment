6495f9823a67672dc737e46acfea375b51dc5444
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-21 17:06:41 +0530

    SumOfAllElements function is used
    Tests are changed to test framework

--- test/averageOfElementsTest.js
+++ test/averageOfElementsTest.js
@@ -1,34 +1,19 @@
 const assert = require('assert');
 const lib = require('../lib/averageOfElements.js');
+const { testLib } = require('../testLib.js');
 
-//valid input
-let actualOutput = lib.average([1,2,3,4,5]);
-let expectedOutput = 3;
-assert.equal(actualOutput,expectedOutput);
+console.log("---------- Test Report ----------");
 
-//empty array as an input
-actualOutput = lib.average([]);
-expectedOutput = NaN
-assert.equal(isNaN(actualOutput),isNaN(expectedOutput));
+testLib(lib.average,[1,2,3,4,5],3);
 
-//only one array element
-actualOutput = lib.average([6]);
-expectedOutput = 6;
-assert.equal(actualOutput,expectedOutput);
- 
-//take only one element 
-assert.equal(lib.average([2]),2);
+testLib(lib.average,[6],6);
 
-//check with 0
-assert.equal(lib.average([0,2]),1);
+testLib(lib.average,[0,2],1);
 
-//check with negative numbers
-assert.equal(lib.average([1,-5]),-2);
+testLib(lib.average,[1,-5],-2);
 
-//check with same number
-assert.equal(lib.average([1,1]),1);
+testLib(lib.average,[1,1],1);
 
-//check with decial point
-assert.equal(lib.average([3,5,6,2,9,4]),4.83);
+testLib(lib.average,[3,5,6,2,9,4],4.83);
 
-
+testLib(lib.average,[],'NaN');
