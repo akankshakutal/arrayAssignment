eba1be19ed26dce3282c06a3d4a33917cab7046b
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-10-21 16:40:23 +0530

    Input is added to test Framework
    Test framework now also showing input

--- testLib.js
+++ testLib.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 
-const printLog = function(functionName,actualOutput,expectedOutput,message) { 
+const printLog = function(functionName,input,actualOutput,expectedOutput,message) { 
   console.log("Function Name  : ",message,functionName.name);
+  console.log("Input          : ",input);
   console.log("actualOutput   : ",actualOutput);
   console.log("expectedOutput : ",expectedOutput );
   console.log("---------------------------------");
@@ -11,7 +12,7 @@ const testLib = function(functionName,input,expectedOutput) {
   let actualOutput = functionName(input);
   expectedOutput = expectedOutput;
   assert.deepEqual(actualOutput,expectedOutput);
-  printLog(functionName,actualOutput,expectedOutput,"");
+  printLog(functionName,input,actualOutput,expectedOutput,"");
 }
 
 exports.printLog = printLog;
